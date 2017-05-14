24e19df4bc2e80393ea383f923abb4a1461ecdc4
stoyan kostov <stoyankostov@gmail.com>
2017-05-14 10:33:29 +0300

    echo web server added

--- index.js
+++ index.js
@@ -26,3 +26,10 @@ stdin.on('data', function (key) {
 
     if (key == '\u0003') { process.exit(); }    // ctrl-c
 });
+
+// Test
+var http = require('http');
+http.createServer(function (request, response) {
+    response.writeHead(200);
+    request.pipe(response);
+}).listen(8080);
