e06000d7b503d73d8fc78ed2fd4a127349f7197b
stoyan kostov <stoyankostov@gmail.com>
2017-05-14 22:10:36 +0300

    Echo pressed keys

--- index.js
+++ index.js
@@ -2,27 +2,29 @@ var stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.resume();
 stdin.setEncoding('utf8');
+var keys = [];
 
 stdin.on('data', function (key) {
-    var fs = require('fs');
+    //var fs = require('fs');
 
-    fs.appendFile('message.txt', key + '\n', function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-    });
-
-    if (key == '\u001B\u005B\u0041') {
-        process.stdout.write('up');
-    }
-    if (key == '\u001B\u005B\u0043') {
-        process.stdout.write('right');
-    }
-    if (key == '\u001B\u005B\u0042') {
-        process.stdout.write('down');
-    }
-    if (key == '\u001B\u005B\u0044') {
-        process.stdout.write('left');
-    }
+    // fs.appendFile('message.txt', key + '\n', function (err) {
+    //     if (err) throw err;
+    //     console.log('Saved!');
+    // });
+    keys.push({ 'key': key });
+    console.log(keys);
+    // if (key == '\u001B\u005B\u0041') {
+    //     process.stdout.write('up');
+    // }
+    // if (key == '\u001B\u005B\u0043') {
+    //     process.stdout.write('right');
+    // }
+    // if (key == '\u001B\u005B\u0042') {
+    //     process.stdout.write('down');
+    // }
+    // if (key == '\u001B\u005B\u0044') {
+    //     process.stdout.write('left');
+    // }
 
     if (key == '\u0003') { process.exit(); }    // ctrl-c
 });
@@ -30,6 +32,9 @@ stdin.on('data', function (key) {
 // Test
 var http = require('http');
 http.createServer(function (request, response) {
-    response.writeHead(200);
-    request.pipe(response);
-}).listen(8080);
+    response.writeHead(200, { "Content-Type": "application/json" });
+    //request.pipe(response);
+    response.end(JSON.stringify(keys));
+}).listen(8080, function(){
+    console.log('Server running');
+});
